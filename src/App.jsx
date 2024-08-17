bd9f8ce7bd45324d4d5a74037b192dac064a7c4c
Abdullah Iftikhar <abdullahiftikharabbasi8@gmail.com>
2024-08-17 14:37:02 +0500

    Adding Tailwind in React App

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<>
+			<h1 className=" font-bold text-blue-400 text-center text-6xl">
+				Hello world!
+			</h1>
+			<p className="text-blue-800 w-[80%]  m-auto mt-5">
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi voluptate
+				aliquid a aperiam quod necessitatibus inventore illum hic porro nobis
+				illo rerum sapiente unde magnam, doloribus eligendi, deserunt corporis
+				ad!
+			</p>
+		</>
+	);
 }
 
-export default App
+export default App;
